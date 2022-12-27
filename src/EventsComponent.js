3040859ef1582f57a139ebc0218e7db6fdaef5c2
Cintia Ruiz <cintiaruizv@gmail.com>
2022-12-27 11:44:01 -0600

    Basic events

--- src/EventsComponent.js
+++ src/EventsComponent.js
@@ -6,10 +6,35 @@ export const EventsComponent = () => {
         alert('hola, soy un evento')
     }
 
+    const handleDoubleClick = (e) => {
+      alert('you clicked me twice!')
+    }
+    const handleHover = (e, action) => {
+      alert('Youve' +' '+ action)
+    }
+    const youreIn = (e) => {
+console.log('youre in input write your name')
+    }
+    const youreOut = (e) => {
+      console.log('youre out')
+    }
+   
   return (
     <div>
         <h2>Eventos en react</h2>
         <button onClick={handleClick}>Click me! </button>
+        <button onDoubleClick={handleDoubleClick} >Double click me</button>
+    <div id="box" 
+    onMouseEnter={e => handleHover(e, 'entered')}
+    onMouseLeave={e => handleHover(e, 'leaved')}>
+    Hover me!
+    </div>
+    <p>
+      <input type="text" 
+      onFocus={youreIn} 
+      onBlur={youreOut}
+      placeholder="Enter your name" />
+    </p>
     </div>
 
   )
